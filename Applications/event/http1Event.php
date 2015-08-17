02e043562bfa7eddc629081abb2b9c99743d4554
will <will@wizmacau.com>
2015-08-17 19:34:40 +0800

    写了几个注释

--- Applications/event/http1Event.php
+++ Applications/event/http1Event.php
@@ -6,9 +6,14 @@
  * Time: 下午6:04
  */
 
+
 namespace Applications\event;
 
-
+/**
+ * Class http1Event
+ * @package Applications\event
+ * 示范一下,事件处理类的命名是随意的,没有限制,只要在生成worker时,new 对象并用setEvent()方法载入即可
+ */
 class http1Event
 {
     function onRequest($request, $response) {
--- example/http_test.php
+++ example/http_test.php
@@ -15,6 +15,7 @@ require_once __DIR__.'/../Autoloader.php';
 // 创建一个Worker监听2345端口，使用http协议通讯
 $http_worker = new Worker("http://0.0.0.0:8888");
 
+// 事件只要在Application中即可,具体放哪,怎么写随意.只要完成要求的事件即可.
 $http_worker->setEvent(new http1Event());// 读取并用户自定义事件
 
 // 启动4个进程对外提供服务
