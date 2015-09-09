4d9de8462a3363c01dc8a022b43becf86fe91b33
will <will@wizmacau.com>
2015-09-09 18:21:48 +0800

    tag 0.0.6

--- Worker.php
+++ Worker.php
@@ -36,7 +36,7 @@ class Worker
     // 业务配置信息
     public $config = array( 'server' => array() );
     // 服务容器的版本号
-    const  VERSION = '0.0.5';
+    const  VERSION = '0.0.6';
     // pid 存储文件
     protected $pid_file;
     // 业务主入口文件名
